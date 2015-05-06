79c5fbfb89efd9240654f95caa187d3a05fc950a
Tr-blank <blank0129@gmail.com>
2015-05-06 21:16:06 +0800

    all

--- javascripts/view.js
+++ javascripts/view.js
@@ -3,18 +3,4 @@ $(function() {
       $(".archives .word iframe").addClass('iframe');
       $(".archives .word em:first-of-type").addClass('em');
     }
-
-    $("#search_box").on('focus',(function(){
-    $(".search .tags").slideDown(200,function(){
-    	$(this).removeAttr("style").addClass("show");
-    });
-      // $(".tags").addClass("show");
-    })).on('blur',(function(){
-        setTimeout(function() {
-          $(".search .tags").slideUp(200,function(){
-          	$(this).removeClass("show").removeAttr("style");
-          });
-        }, 200);
-    }));
-
 })
